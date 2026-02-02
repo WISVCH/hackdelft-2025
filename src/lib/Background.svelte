e3aa4a0fd74d59e4ea58fe11a22a6f7465788926
sdrent <S.Drent@student.tudelft.nl>
2026-02-02 14:15:46 +0100

    about section and general code structure change

---
+++ src/lib/Background.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Sun from "$lib/assets/sun.svg"
+    import Parasols from "$lib/assets/parasols_horizon.svg"
+</script>
+
+<div class="min-h-screen h-full w-screen">
+    <div class="absolute z-1 flex flex-col h-screen w-screen">
+        <div class="bg-CppBlue h-1/5 w-screen">
+            <img class="p-4 h-full" src={Sun} alt="HackDelft" />
+        </div>
+        <div class="bg-CBlue w-screen grow flex flex-col justify-end align-bottom">
+            <img class="z-3 w-screen p-0 border-b-8 border-PineappleYellow align-bottom" src={Parasols} alt=""/>
+            <div class=" align-top bg-PineappleYellow p-0 h-33 w-screen"></div>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
+    import Background from "$lib/Background.svelte"
     import StartingScreen from "$lib/StartingScreen.svelte";
-
+    import About from "$lib/About.svelte";
 </script>
 
-<StartingScreen />
+<div class="absolute z-1">
+    <Background />
+</div>
+<div class="absolute z-2">
+    <StartingScreen />
+    <About/>
+</div>
